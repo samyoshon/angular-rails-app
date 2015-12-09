d0619937dae0e55373f3ae3c4fcc7c5b23bc3ef0
samyoshon <samuelyshon@gmail.com>
2015-12-09 10:52:03 -0800

    controller - prevents blank entry

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 angular.module('flapperNews', [])
 	.controller('MainCtrl', [
 		'$scope', function($scope) {
+			//Properties and Methods
 			$scope.test = "Hello World";
+			$scope.posts = [
+				{title: 'post 1', upvotes: 5},
+				{title: 'post 2', upvotes: 2},
+				{title: 'post 3', upvotes: 15},
+				{title: 'post 4', upvotes: 9},
+				{title: 'post 5', upvotes: 4}
+			];
+			$scope.addPost = function() {
+				if (!$scope.title || $scope.title === '') {
+					return;
+				}
+				$scope.posts.push({title: $scope.title, upvotes: 0});
+				$scope.title = '';
+			};
 		}
 	]);
