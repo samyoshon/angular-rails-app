656a7145ca8304fe2287e55d5e5d7b7b5b58991b
samyoshon <samuelyshon@gmail.com>
2015-12-14 10:30:43 -0800

    job post countries added

--- js/controllers/loginCtrl.js
+++ js/controllers/loginCtrl.js
@@ -5,16 +5,17 @@ angular.module('jetPath')
 
 		console.log('this is login ctrl');
 
+		//USER is being passed from form
 		$scope.loginCandidate = function(user) {
 			$scope.candidate.email = user.email;
 			$scope.candidate.password = user.password;
-			//NEED TO SEND THIS DATA TO RAILS TO AUTHENTICATE
+			//NEED TO SEND CANDIDATE OBJECT TO RAILS TO AUTHENTICATE
 		};
 
 		$scope.loginEmployer = function(user) {
 			$scope.employer.email = user.email;
 			$scope.employer.password = user.password;
-			//NEED TO SEND THIS DATA TO RAILS TO AUTHENTICATE
+			//NEED TO SEND EMPLOYER OBJECT TO RAILS TO AUTHENTICATE
 		};
 
 	});
--- js/controllers/postCtrl.js
+++ js/controllers/postCtrl.js
@@ -7,13 +7,20 @@ angular.module('jetPath')
 	
 	$scope.postJob = function(job) {
 			$scope.job.title = job.title;
-			$scope.job.password = user.password;
-			$scope.job.password = user.password;
-			$scope.job.password = user.password;
-			$scope.job.password = user.password;
-			$scope.job.password = user.password;
-			$scope.job.password = user.password;
+			$scope.job.description = job.description;
+			$scope.job.city = job.city;
+			$scope.job.country = job.country;
+			$scope.job.qualifications = job.qualifications;
+			$scope.job.low = job.low;
+			$scope.job.high = job.high;
+			$scope.job.benefits = job.benefits;
+			$scope.job.airfare = job.airfare;
+			$scope.job.housing = job.housing;
+			$scope.job.medical = job.medical;
+			$scope.job.pto = job.pto;
+			$scope.job.sick = job.sick;
+			$scope.job.additionalinfo = job.additionalinfo;
+			$scope.job.apply = job.apply;
 	};
-
 
 });
